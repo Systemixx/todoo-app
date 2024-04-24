3a43ffe3c6d79a88d06902566fae6f6b8e399e5b
Chris Neidl <chris.neidl@gmx.de>
2024-04-24 15:07:35 +0200

    update

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,19 @@ import todoRoutes from './routes/todoRoutes';
 const app = express();
 
 app.use(express.json());
+
+// Routen für TODOs
 app.use('/todos', todoRoutes);
+
+// Middleware für die Root-Route
+app.get('/', (req, res) => {
+  res.send('Willkommen zur TODO-App!');
+});
+
+// Middleware für 404-Fehlerbehandlung
+app.use((req, res) => {
+  res.status(404).send('404 - Seite nicht gefunden');
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
